faf247d04b134453054c9e60ed161b5702b4a848
prkhrbhsn <pbhasin400@gmail.com>
2021-06-13 01:54:51 +0530

    chat body progress

--- src/Components/ChatList/ChatList.js
+++ src/Components/ChatList/ChatList.js
@@ -12,9 +12,33 @@ export default function ChatPanel() {
         lastMessage='LOL'
         Time='20:13'
         senderImage='https://avatars.githubusercontent.com/u/61727284?v=4'
+        active={false}
+      />
+      <RecentChat
+        senderName='Ishit Beswal'
+        lastSender='Ishit'
+        lastMessage='Noicee'
+        Time='19:53'
+        senderImage='https://avatars.githubusercontent.com/u/53562523?v=4'
         active={true}
       />
       <RecentChat
+        senderName='Kirtik Singh'
+        lastSender='Kirtik'
+        lastMessage='HAHAHAHAHA'
+        Time='17:45'
+        senderImage='https://avatars.githubusercontent.com/u/42700919?v=4'
+        active={false}
+      />
+      <RecentChat
+        senderName='Apoorva Nautiyal'
+        lastSender='You'
+        lastMessage='Hellloooo'
+        Time='15:03'
+        senderImage='https://raw.githubusercontent.com/prkhrbhsn/CloudTunes/master/Front-End/myapp/assets/Appu.png'
+        active={false}
+      />
+      {/* <RecentChat
         senderName='Ishit Beswal'
         lastSender='Ishit'
         lastMessage='Noicee'
@@ -77,23 +101,7 @@ export default function ChatPanel() {
         Time='15:03'
         senderImage='https://raw.githubusercontent.com/prkhrbhsn/CloudTunes/master/Front-End/myapp/assets/Appu.png'
         active={false}
-      />
-      <RecentChat
-        senderName='Ishit Beswal'
-        lastSender='Ishit'
-        lastMessage='Noicee'
-        Time='19:53'
-        senderImage='https://avatars.githubusercontent.com/u/53562523?v=4'
-        active={false}
-      />
-      <RecentChat
-        senderName='Apoorva Nautiyal'
-        lastSender='You'
-        lastMessage='Hellloooo'
-        Time='15:03'
-        senderImage='https://raw.githubusercontent.com/prkhrbhsn/CloudTunes/master/Front-End/myapp/assets/Appu.png'
-        active={false}
-      />
+      /> */}
       {/* <RecentChat />
       <RecentChat /> */}
     </div>
--- src/Views/ChatPage/ChatPage.js
+++ src/Views/ChatPage/ChatPage.js
@@ -10,7 +10,105 @@ export default function ChatPage() {
       <div className='sidebar'></div>
       <ChatList />
       <div className='chat-body'>
-        <h1>Chat Body</h1>
+        <div className='active-chat-header'>
+          <img
+            className='active-chat-image'
+            src='https://avatars.githubusercontent.com/u/53562523?v=4'
+            alt='active'
+          />
+          <p className='active-chat-contact'>Ishit Beswal</p>
+        </div>
+        <div className='chat-messages'>
+          <div className='chat-msg-container you'>
+            <img
+              src='https://avatars.githubusercontent.com/u/53562523?v=4'
+              alt=''
+              className='in-chat-image'
+            />
+            <p className='chat-message you'>Hi Ishit, How are you?</p>
+          </div>
+
+          <div className='chat-msg-container sender'>
+            <img
+              src='https://avatars.githubusercontent.com/u/53562523?v=4'
+              alt=''
+              className='in-chat-image'
+            />
+            <p className='chat-message sender'>I'm good, what's up?</p>
+          </div>
+          <div className='chat-msg-container you'>
+            <img
+              src='https://avatars.githubusercontent.com/u/53562523?v=4'
+              alt=''
+              className='in-chat-image'
+            />
+            <p className='chat-message you'>I got an award today!</p>
+          </div>
+          <div className='chat-msg-container sender'>
+            <img
+              src='https://avatars.githubusercontent.com/u/53562523?v=4'
+              alt=''
+              className='in-chat-image'
+            />
+            <p className='chat-message sender'>I'm good, what's up?</p>
+          </div>
+          <div className='chat-msg-container you'>
+            <img
+              src='https://avatars.githubusercontent.com/u/53562523?v=4'
+              alt=''
+              className='in-chat-image'
+            />
+            <p className='chat-message you'>I got an award today!</p>
+          </div>
+          <div className='chat-msg-container sender'>
+            <img
+              src='https://avatars.githubusercontent.com/u/53562523?v=4'
+              alt=''
+              className='in-chat-image'
+            />
+            <p className='chat-message sender'>I'm good, what's up?</p>
+          </div>
+          <div className='chat-msg-container you'>
+            <img
+              src='https://avatars.githubusercontent.com/u/53562523?v=4'
+              alt=''
+              className='in-chat-image'
+            />
+            <p className='chat-message you'>I got an award today!</p>
+          </div>
+          <div className='chat-msg-container sender'>
+            <img
+              src='https://avatars.githubusercontent.com/u/53562523?v=4'
+              alt=''
+              className='in-chat-image'
+            />
+            <p className='chat-message sender'>I'm good, what's up?</p>
+          </div>
+          <div className='chat-msg-container you'>
+            <img
+              src='https://avatars.githubusercontent.com/u/53562523?v=4'
+              alt=''
+              className='in-chat-image'
+            />
+            <p className='chat-message you'>I got an award today!</p>
+          </div>
+          <div className='chat-msg-container sender'>
+            <img
+              src='https://avatars.githubusercontent.com/u/53562523?v=4'
+              alt=''
+              className='in-chat-image'
+            />
+            <p className='chat-message sender'>Oh wow!</p>
+          </div>
+          <div className='chat-msg-container sender'>
+            <img
+              src='https://avatars.githubusercontent.com/u/53562523?v=4'
+              alt=''
+              className='in-chat-image'
+            />
+            <p className='chat-message sender'>Noicee</p>
+          </div>
+        </div>
       </div>
     </div>
   );
