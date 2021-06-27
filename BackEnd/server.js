2738be90121d5d917a5bfeab1c005ed666986445
prkhrbhsn <pbhasin400@gmail.com>
2021-06-27 23:54:50 +0530

    init passport auth

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const passport = require("passport");
+const session = require("express-session");
 const connectDB = require("./config/db");
 dotenv.config({ path: "./config/config.env" });
 
 connectDB();
+
+server.use(
+  session({
+    secret: "Prakhars chat app",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+server.use(passport.initialize());
+server.use(passport.session());
 
 const server = express();
 
